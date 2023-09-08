0edf43e51b5217f0f7d7df33aaa682f3bb5fe1e4
PavelchukMax <120489328+PavelchukMax@users.noreply.github.com>
2023-09-08 13:11:07 +0300

    Create Company.cs

---
+++ lab1/lab1/Company.cs
@@ -0,0 +1,15 @@
+namespace lab1
+{
+    class Company
+    {
+        public string name;
+        public int nworkers;
+        public string description;
+        public Company(string name, int nworkers, string description)
+        {
+            this.name = name;
+            this.nworkers = nworkers;
+            this.description = description;
+        }
+    }
+}
