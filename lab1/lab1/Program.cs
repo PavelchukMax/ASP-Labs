87c519fa8eda54f31e8bd5fe6558fe26fa835f19
PavelchukMax <120489328+PavelchukMax@users.noreply.github.com>
2023-09-08 13:09:41 +0300

    Update Program.cs

--- lab1/lab1/Program.cs
+++ lab1/lab1/Program.cs
@@ -1,7 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 Random x = new Random();
-Company company= new Company("Microsoft",5000, "This is the company that created Windows");
+lab1.Company company = new lab1.Company("Microsoft",5000, "This is the company that created Windows");
 
 app.MapGet("1/", () => "Hello World!");
 app.MapGet("2/", () => $"Company name {company.name} Company nworkers {company.nworkers} Company description {company.description}");
@@ -11,15 +11,3 @@ app.MapGet("3/", () => $"random number 0-100: {n}");
 
 
 app.Run();
-class Company
-{
-    public string name;
-    public int nworkers;
-    public string description;
-    public Company(string name, int nworkers, string description)
-    {
-        this.name = name;
-        this.nworkers = nworkers;
-        this.description = description;
-    }
-}
