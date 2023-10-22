90ab8b104c1e907c1c818f0e89d601bfec65eaa0
PavelchukMax <120489328+PavelchukMax@users.noreply.github.com>
2023-10-22 17:20:29 +0300

    Add files via upload

---
+++ lab 7/Controllers/DownloadFileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Web;
+
+    public class FileController : Controller
+    {
+    [HttpGet]
+    public IActionResult DownloadFile()
+    {
+        return View();
+    }
+
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            string eFileName = HttpUtility.UrlEncode(fileName);
+            string contentOfFile = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+            byte[] bytes = Encoding.UTF8.GetBytes(contentOfFile);
+
+            Response.Headers.Add("Content-Disposition", $"attachment; filename={eFileName}.txt");
+            Response.ContentType = "text/plain";
+
+            return File(bytes, "text/plain");
+        }
+    }
