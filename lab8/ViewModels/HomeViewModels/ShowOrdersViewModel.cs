912f33fd7146472a237f178c5d5ece7c966ba487
PavelchukMax <120489328+PavelchukMax@users.noreply.github.com>
2023-10-29 17:27:17 +0200

    Add files via upload

---
+++ lab8/ViewModels/HomeViewModels/ShowOrdersViewModel.cs
@@ -0,0 +1,11 @@
+﻿using lab8.Models;
+
+namespace lab8.ViewModels.HomeViewModels
+{
+    public enum ShowStyles
+    {
+        List,
+        Table
+    }
+    public record class ShowOrdersViewModel(IEnumerable<Order> Orders, ShowStyles ShowStyle);
+}
