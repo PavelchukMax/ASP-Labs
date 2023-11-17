27ab3d3c3ec7ef29a4fde97e13db4febdccc8bf6
PavelchukMax <120489328+PavelchukMax@users.noreply.github.com>
2023-11-17 14:58:10 +0200

    Add files via upload

---
+++ lab_12_1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ConsoleApp1;
+class Program
+{
+    static void Main()
+    {
+        using (var context = new AppDbContext())
+        {
+            context.Database.EnsureCreated();
+        }
+
+        // Додайте 3 елементи до бази даних
+        AddUsers();
+
+        // Витягніть дані з бази даних та виведіть на консоль
+        DisplayUsers();
+    }
+
+    static void AddUsers()
+    {
+        using (var context = new AppDbContext())
+        {
+            var users = new[]
+            {
+                new User { FirstName = "Іван", LastName = "Іванов", Age = 25 },
+                new User { FirstName = "Петро", LastName = "Петров", Age = 30 },
+                new User { FirstName = "Марія", LastName = "Сидорова", Age = 22 }
+            };
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+        }
+    }
+
+    static void DisplayUsers()
+    {
+        using (var context = new AppDbContext())
+        {
+            var users = context.Users.ToList();
+
+            Console.WriteLine("Список користувачів:");
+            foreach (var user in users)
+            {
+                Console.WriteLine($"Ім'я: {user.FirstName}, Прізвище: {user.LastName}, Вік: {user.Age}");
+            }
+        }
+    }
+}
